4540181c1499ee69262585d89acfc5e22080912b
Mu Qin <magenta2127@gmail.com>
2019-12-01 18:01:54 +0800

    add section model

---
+++ src/section/section_model.js
@@ -0,0 +1,39 @@
+const schema = {
+  type: 'object',
+  title: 'Section Model',
+  required: [
+  ],
+  properties: {
+    id: {
+      type: 'minimum',
+      minimum: 1,
+      description: 'auto increment section id',
+    },
+    title: {
+      type: 'string',
+      minLength: 1,
+      maxLength: 50,
+      format: 'ascii_printable',
+      description: 'title of section',
+    },
+    pages: {
+
+    },
+    created_at: {
+      "type": "string",
+      "format": "date-time",
+      "example": "2019-12-01T16:39:57-08:00",
+      "description": "time created at"
+    },
+    updated_at: {
+      "type": "string",
+      "format": "date-time",
+      "example": "2019-12-01T16:39:57-08:00",
+      "description": "time updated at"
+    },
+  },
+}
+
+module.exports = {
+  schema,
+};
