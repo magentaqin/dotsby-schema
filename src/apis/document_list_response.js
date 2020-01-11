37ad13a439951f3f5b486faba6dda3772eb4f858
Mu Qin <magenta2127@gmail.com>
2020-01-11 10:10:01 +0800

    add all_versions field for document list response

--- src/apis/document_list_response.js
+++ src/apis/document_list_response.js
@@ -1,4 +1,5 @@
 const documentSchema = require('../document/type_document');
+const { version_regx } = require('../../utils/types');
 const { omitKeys } = require('../../utils/objHelper');
 const { omitArrElements } = require('../../utils/arrayHelper');
 
@@ -6,6 +7,20 @@ const document = JSON.parse(JSON.stringify(documentSchema.schema));
 
 document.required = omitArrElements(document.required, ['sections', 'user_id']);
 document.properties = omitKeys(document.properties, ['sections', 'user_id']);
+
+document.required = [...document.required, 'all_versions'];
+document.properties = {
+  ...document.properties,
+  all_versions: {
+    type: 'array',
+    minItems: 1,
+    items: {
+      type: 'string',
+      pattern: version_regx,
+      description: 'version of document',
+    }
+  }
+}
 
 const schema = {
   title: 'Get Document List Response',
